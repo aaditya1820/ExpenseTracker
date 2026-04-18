8e810ec0eeee672d1c04b8676825caf3b8bf4cdc
aaditya7670 <aadimanishajitu1122@gmail.com>
2026-04-18 16:09:23 +0530

    Fix missing icon LuBarChart3 for production build

--- frontend/src/components/Dashboard/Last30DaysExpences.jsx
+++ frontend/src/components/Dashboard/Last30DaysExpences.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { prepareExpenseBarChartData } from '../../utils/helper';
 import CustomBarChart from '../Charts/CustomBarChart';
-import { LuBarChart3 } from "react-icons/lu";
+import { LuChartBar } from "react-icons/lu";
 
 const Last30DaysExpenses = ({ data }) => {
   const [chartData, setChartData] = useState([]);
@@ -14,7 +14,7 @@ const Last30DaysExpenses = ({ data }) => {
     <div className="card col-span-1 border border-blue-50">
       <div className="flex items-center justify-between mb-6">
         <h5 className="text-xl font-bold text-blue-900 flex items-center gap-2">
-          <LuBarChart3 className="text-blue-500" />
+          <LuChartBar className="text-blue-500" />
           Monthly Expenses
         </h5>
       </div>
